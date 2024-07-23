3cca41bcad0e6befdd0231a9f7f7a6ae45e53640
Tommy Sørbråten <tommysor@gmail.com>
2024-07-23 18:37:59 +0000

    Remove fort

--- fort/src/app/_components/fortState.ts
+++
@@ -1,8 +0,0 @@
-const tileColumnCount = 3;
-const tileRowCount = 3;
-
-export const initialFortState = {
-    time: 0,
-    tileColsCount: tileColumnCount,
-    tileRowsCount: tileRowCount,
-}
--- fort/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import FortBoard from "./_components/fortBoard";
-
-export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <FortBoard />
-    </main>
-  );
-}
