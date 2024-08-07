85fed249db76ea9defaafb93eed4d975495f1165
Tommy Sørbråten <tommysor@gmail.com>
2024-08-07 18:58:44 +0000

    Hello typescript

---
+++ reatam/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function dothis() {
+    var elem = document.getElementById("hello-world");
+    if (elem) {
+        elem.innerHTML = "Hello typescript!";
+    }
+}
+dothis();
---
+++ reatam/src/index.ts
@@ -0,0 +1,7 @@
+function dothis() {
+  var elem = document.getElementById("hello-world");
+  if (elem) {
+    elem.innerHTML = "Hello typescript!";
+  }
+}
+dothis();
