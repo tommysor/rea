4b50c56588b365016f8832087ab07bfa271acc32
Tommy Sørbråten <tommysor@gmail.com>
2024-08-09 18:41:31 +0200

    Format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react'
-import { createTam, TamUnit, idleTam } from './tam/tam'
-import Tam from './tam/Tam'
+import { useState, useEffect } from "react";
+import { createTam, TamUnit, idleTam } from "./tam/tam";
+import Tam from "./tam/Tam";
 
 export default function App() {
   const [tams, setTams] = useState<TamUnit[]>([]);
   useEffect(() => {
     const interval = setInterval(() => {
-      setTams(prevTams => {
-        const updatedTams = prevTams.map(tam => idleTam(tam));
+      setTams((prevTams) => {
+        const updatedTams = prevTams.map((tam) => idleTam(tam));
         return updatedTams;
       });
     }, 1000);
@@ -28,19 +28,14 @@ export default function App() {
 
   return (
     <>
-      <p className='text-3xl font-bold underline'>
-        Hello reatam
-      </p>
-      <button onClick={addNewTam}>
-        Add Tam
-      </button>
-      <button onClick={clearTams}>
-        Clear
-      </button>
-      {
-        tams.map(tam => (
-          <div id={tam.id}><Tam tam={tam} /></div>))
-      }
+      <p className="text-3xl font-bold underline">Hello reatam</p>
+      <button onClick={addNewTam}>Add Tam</button>
+      <button onClick={clearTams}>Clear</button>
+      {tams.map((tam) => (
+        <div id={tam.id}>
+          <Tam tam={tam} />
+        </div>
+      ))}
     </>
-  )
+  );
 }
--- src/tam/tam.ts
+++ src/tam/tam.ts
@@ -1,32 +1,32 @@
 export type TamUnit = {
-    id: string;
-    age: number;
-    foodLevel: number;
+  id: string;
+  age: number;
+  foodLevel: number;
 };
 
 const foodLevelMax = 100;
 
-export function createTam({ id }: { id: string}): TamUnit {
-    return {
-        id: id,
-        age: 0,
-        foodLevel: foodLevelMax,
-    };
+export function createTam({ id }: { id: string }): TamUnit {
+  return {
+    id: id,
+    age: 0,
+    foodLevel: foodLevelMax,
+  };
 }
 
 export function idleTam(tam: TamUnit): TamUnit {
-    const newFoodLevel = tam.foodLevel - 1;
-    return {
-        ...tam,
-        age: tam.age + 1,
-        foodLevel: newFoodLevel < 0 ? 0 : newFoodLevel,
-    };
+  const newFoodLevel = tam.foodLevel - 1;
+  return {
+    ...tam,
+    age: tam.age + 1,
+    foodLevel: newFoodLevel < 0 ? 0 : newFoodLevel,
+  };
 }
 
 export function feedTam(tam: TamUnit): TamUnit {
-    const newFoodLevel = tam.foodLevel + 10;
-    return {
-        ...tam,
-        foodLevel: newFoodLevel > foodLevelMax ? foodLevelMax : newFoodLevel,
-    };
+  const newFoodLevel = tam.foodLevel + 10;
+  return {
+    ...tam,
+    foodLevel: newFoodLevel > foodLevelMax ? foodLevelMax : newFoodLevel,
+  };
 }
