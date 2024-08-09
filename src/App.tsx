8dba5557d4f2848580e8a6db3a9bf30ebac66e34
Tommy Sørbråten <tommysor@gmail.com>
2024-08-09 17:26:54 +0200

    Add gameloop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
-import { createTam, TamUnit } from './tam/tam'
+import { useState, useEffect } from 'react'
+import { createTam, TamUnit, idleTam } from './tam/tam'
 import Tam from './tam/Tam'
 
 export default function App() {
   const [tams, setTams] = useState<TamUnit[]>([]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTams(prevTams => {
+        const updatedTams = prevTams.map(tam => idleTam(tam));
+        return updatedTams;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   function addNewTam() {
     const rnd = Math.random();
     const id = Math.ceil(rnd * 1_000_000);
