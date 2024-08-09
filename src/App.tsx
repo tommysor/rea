08006bb9b29e262d1338968c074475944804c862
Tommy Sørbråten <tommysor@gmail.com>
2024-08-09 21:03:08 +0200

    Introduce World wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,12 @@
-import { useState, useEffect } from "react";
-import { createTam, TamUnit, idleTam, feedTam } from "./tam/tam";
-import Tam from "./tam/Tam";
+import World from "./World";
 
 export default function App() {
-  const [tams, setTams] = useState<TamUnit[]>([]);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTams((prevTams) => {
-        const updatedTams = prevTams.map((tam) => {
-          const rnd = Math.random();
-          if (rnd < 0.095) {
-            return feedTam(tam);
-          } else {
-            return idleTam(tam);
-          }
-        });
-        return updatedTams;
-      });
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-  function addNewTam() {
-    const rnd = Math.random();
-    const id = Math.ceil(rnd * 1_000_000);
-    const newTam = createTam({ id: id.toString() });
-    setTams([...tams, newTam]);
-  }
-  function clearTams() {
-    setTams([]);
-  }
-
   return (
     <>
       <h1 className="flex items-center justify-center text-3xl font-bold">
         ReaTam
       </h1>
-      <div className="flex items-center justify-center">
-        <button
-          onClick={addNewTam}
-          className="rounded-md bg-stone-500 mx-1 my-1 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Add Tam
-        </button>
-        <button
-          onClick={clearTams}
-          className="rounded-md bg-stone-500 mx-1 my-1 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Clear
-        </button>
-      </div>
-      {tams.map((tam) => (
-        <div key={tam.id}>
-          <Tam tam={tam} />
-        </div>
-      ))}
+      <World />
     </>
   );
 }
---
+++ src/World.tsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { TamUnit, createTam, idleTam, feedTam } from "./tam/tam";
+import Tam from "./tam/Tam";
+
+export default function World() {
+  const [worldTam] = useState(createTam({ id: "0" }));
+  const [tams, setTams] = useState<TamUnit[]>([]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTams((prevTams) => {
+        const updatedTams = prevTams.map((tam) => {
+          const rnd = Math.random();
+          if (rnd < 0.095) {
+            return feedTam(tam);
+          } else {
+            return idleTam(tam);
+          }
+        });
+        return updatedTams;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  function addNewTam() {
+    const id = Math.ceil(Math.random() * 1_000_000);
+    const newTam = createTam({ id: id.toString() });
+    setTams([...tams, newTam]);
+  }
+  function clearTams() {
+    setTams([]);
+  }
+  return (
+    <div>
+      <div className="bg-neutral-300 rounded-3xl my-4 mx-4 py-4 sm:py-16">
+        <div className="mx-auto max-w-7xl px-4 lg:px-8">
+          <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+            <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+              <dt className="order-first text-xs leading-1 text-gray-600">
+                Id: {worldTam.id}
+              </dt>
+              <div className="flex items-center justify-center">
+                <button
+                  onClick={addNewTam}
+                  className="rounded-md bg-stone-500 mx-1 my-1 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Add Tam
+                </button>
+                <button
+                  onClick={clearTams}
+                  className="rounded-md bg-stone-500 mx-1 my-1 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Clear
+                </button>
+              </div>
+            </div>
+          </dl>
+        </div>
+      </div>
+
+      {tams.map((tam) => (
+        <div key={tam.id}>
+          <Tam tam={tam} />
+        </div>
+      ))}
+    </div>
+  );
+}
