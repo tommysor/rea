64be794c06dc6d3191fdfcb410dc49eb8c065ad7
Tommy Sørbråten <tommysor@gmail.com>
2024-08-10 18:25:47 +0200

    move godDecision actions to own methods

--- src/tam/godtam.ts
+++ src/tam/godtam.ts
@@ -6,22 +6,29 @@ export type GodTamUnit = {
 
 export function createGodTam(): GodTamUnit {
   return {
-    tams: [] as TamUnit[],
+    tams: [],
   };
 }
 
 export function godDecision(godTam: GodTamUnit): GodTamUnit {
   if (godTam.tams.length === 0) {
-    const id = Math.ceil(Math.random() * 1_000_000);
-    return {
-      ...godTam,
-      tams: [...godTam.tams, createTam({ id: id.toString() })],
-    };
+    return godWithNewTam(godTam);
   }
 
+  return godWithOneLessDeadTam(godTam);
+}
+
+export function godWithNewTam(godTam: GodTamUnit): GodTamUnit {
+  const id = Math.ceil(Math.random() * 1_000_000);
+  return {
+    ...godTam,
+    tams: [...godTam.tams, createTam({ id: id.toString() })],
+  };
+}
+
+function godWithOneLessDeadTam(godTam: GodTamUnit): GodTamUnit {
   const newGodTam = { ...godTam, tams: [...godTam.tams] };
   const deadTam = godTam.tams.find((tam) => tam.foodLevel <= 0);
   newGodTam.tams = godTam.tams.filter((tam) => tam.id != deadTam?.id);
-
   return newGodTam;
 }
