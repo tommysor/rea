1202214fe7242bbe80d733e8ba7599c91e412892
Tommy Sørbråten <tommysor@gmail.com>
2024-08-19 22:33:49 +0200

    Extract Godtam

--- src/World.tsx
+++ src/World.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { createTam, idleTam, feedTam } from "./tam/tam";
+import Godtam from "./tam/Godtam";
 import Tam from "./tam/Tam";
 import { createGodTam, godDecision } from "./tam/godtam";
 import { ReactFlow, Background, BackgroundVariant } from "@xyflow/react";
@@ -57,29 +58,7 @@ export default function World() {
 
   return (
     <div>
-      <div className="bg-neutral-300 rounded-3xl my-4 mx-4 py-4 sm:py-16">
-        <div className="mx-auto max-w-7xl px-4 lg:px-8">
-          <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
-            <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-              <dt className="order-first text-xs leading-1 text-gray-600">-</dt>
-              <div className="flex items-center justify-center">
-                <button
-                  onClick={addNewTam}
-                  className="rounded-md bg-stone-500 mx-1 my-1 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Add Tam
-                </button>
-                <button
-                  onClick={clearTams}
-                  className="rounded-md bg-stone-500 mx-1 my-1 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Clear
-                </button>
-              </div>
-            </div>
-          </dl>
-        </div>
-      </div>
+      <Godtam addNewTam={addNewTam} clearTams={clearTams} />
       <div className="grid grid-cols-5">
         {worldTam.tams.map((tam) => (
           <div key={tam.id} className="grid-flow-col">
---
+++ src/tam/Godtam.tsx
@@ -0,0 +1,32 @@
+export default function Godtam({
+  addNewTam,
+  clearTams,
+}: {
+  addNewTam: () => void;
+  clearTams: () => void;
+}) {
+  return (
+    <div className="bg-neutral-300 rounded-3xl my-4 mx-4 py-4 sm:py-16">
+      <div className="mx-auto max-w-7xl px-4 lg:px-8">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+          <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+            <div className="flex items-center justify-center">
+              <button
+                onClick={addNewTam}
+                className="rounded-md bg-stone-500 mx-1 my-1 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Add Tam
+              </button>
+              <button
+                onClick={clearTams}
+                className="rounded-md bg-stone-500 mx-1 my-1 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Clear
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
